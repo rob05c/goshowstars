7dba29059777e6f2abf9c6b334796d82007391a2
Robert Butts <robert.o.butts@gmail.com>
2014-11-01 13:42:53 -0500

    added files for basic interface

---
+++ main.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"time"
+//	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"strconv"
+	"html/template"
+)
+
+const version = "0.0.0"
+
+const indexFile = "index.html"
+const starTemplateFile = "startemplate.html"
+
+var port uint
+var dataservice string
+
+func init() {
+	const (
+		portDefault          = 0
+		portUsage            = "http serve port"
+		dataserviceDefault   = ""
+		dataserviceUsage     = "URL for data service"
+	)
+	flag.UintVar(&port, "port", portDefault, portUsage)
+	flag.UintVar(&port, "p", portDefault, portUsage+" (shorthand)")
+	flag.StringVar(&dataservice, "data-service", dataserviceDefault, dataserviceUsage)
+	flag.StringVar(&dataservice, "d", dataserviceDefault, dataserviceUsage+" (shorthand)")
+}
+
+func printUsage() {
+	exeName := os.Args[0]
+	fmt.Println(exeName + " " + version + " usage: ")
+	fmt.Println("\t" + exeName + "-d data-service-url -p serve-port")
+	fmt.Println("flags:")
+	flag.PrintDefaults()
+	fmt.Println("example:\n\t" + exeName + "-d 192.168.0.42 -p 80")
+}
+
+type Star struct {
+	Id                int64   `json:"id"`
+	Name              string  `json:"name"`
+	X                 float64 `json:"x"`
+	Y                 float64 `json:"y"`
+	Z                 float64 `json:"z"`
+	Color             float32 `json:"color"`
+	AbsoluteMagnitude float32 `json:"absolute-magnitude"`
+	Spectrum          string  `json:"spectrum"`
+}
+
+func (star *Star) Json() []byte {
+	bytes, err := json.Marshal(star)
+	if err != nil {
+		return nil ///< @todo fix to return JSON error
+	}
+	return bytes
+}
+
+func JsonToStar(starjson []byte) (Star, error) {
+	var star Star
+	err := json.Unmarshal(starjson, &star)
+	if err != nil {
+		return Star {}, err ///< @todo fix to return JSON error
+	}
+	return star, nil
+}
+
+
+type NullStar struct {
+	Id                sql.NullInt64
+	Name              sql.NullString
+	X                 sql.NullFloat64
+	Y                 sql.NullFloat64
+	Z                 sql.NullFloat64
+	Color             sql.NullFloat64
+	AbsoluteMagnitude sql.NullFloat64
+	Spectrum          sql.NullString
+}
+
+/// returns a Star, with zero values for any null values in the NullStar
+func (nstar *NullStar) Star() Star {
+	var star Star
+	if nstar.Id.Valid {
+		star.Id = nstar.Id.Int64
+	}
+	if nstar.Name.Valid {
+		star.Name = nstar.Name.String
+	}
+	if nstar.X.Valid {
+		star.X = nstar.X.Float64
+	}
+	if nstar.Y.Valid {
+		star.Y = nstar.Y.Float64
+	}
+	if nstar.Z.Valid {
+		star.Z = nstar.Z.Float64
+	}
+	if nstar.Color.Valid {
+		star.Color = float32(nstar.Color.Float64)
+	}
+	if nstar.AbsoluteMagnitude.Valid {
+		star.AbsoluteMagnitude = float32(nstar.AbsoluteMagnitude.Float64)
+	}
+	if nstar.Spectrum.Valid {
+		star.Spectrum = nstar.Spectrum.String
+	}
+	return star
+}
+
+func getstar(id int64) (Star, error) {
+	response, err := http.Get("http://" + dataservice + "/star/" + strconv.FormatInt(id, 10))
+	if err != nil {
+		return Star{}, err
+	}
+
+	starjson, err := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+	if err != nil {
+		return Star{}, err
+	}
+
+	star, err := JsonToStar(starjson)
+	if err != nil {
+		return Star{}, err
+	}
+
+	return star, nil
+}
+
+func reachable(uri string) bool {
+	return true
+	timeout := time.Second * 10
+	_, err := net.DialTimeout("tcp", uri, timeout)
+	return err == nil
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	flag.Parse()
+
+	if dataservice == "" {
+			printUsage()
+	}
+
+	if(len(dataservice) > len("http://") && dataservice[0:len("http://")] == "http://") {
+		dataservice = dataservice[len("http://"):]
+	}
+
+	if !reachable(dataservice) {
+		fmt.Println("Could not reach data service " + dataservice)
+		return
+	}
+
+	indexHtml, err := ioutil.ReadFile(indexFile)
+	if err != nil {
+		fmt.Println("Error reading index file: ")
+		fmt.Println(err)
+		return
+	}
+
+	starTemplate, err := template.ParseFiles(starTemplateFile)
+	if err != nil {
+		fmt.Println("Error reading star template file: ")
+		fmt.Println(err)
+		return
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Serving request to " + r.URL.Path)
+		w.Header().Add("Content-Type", "text/html")
+		w.Header().Add("Content-Length", strconv.Itoa(len(indexHtml)))
+		_, err = w.Write(indexHtml)
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+
+	http.HandleFunc("/star/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Serving request to " + r.URL.Path)
+
+		staridStr := r.URL.Path[len("/star/"):]
+		starid, err := strconv.ParseInt(staridStr, 10, 64)
+		_, err = strconv.Atoi(staridStr)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		star, err := getstar(starid)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		starTemplate.Execute(w, star)
+	})
+
+	fmt.Println("Serving on " + strconv.Itoa(int(port)) + "...")
+	http.ListenAndServe(":"+strconv.Itoa(int(port)), nil)
+}
